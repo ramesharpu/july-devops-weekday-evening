24a29c45a7dc2ca6e848765764b09ce9d5eadbd4
Ramesh A <ramesharpu@gmail.com>
2025-07-17 21:17:59 +0530

    added m1 method

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello There!!!");
-
+	}
+	
+	//code changes from b1 branch
+	public void m1() {
+		System.out.println("from method-1");
 	}
 
 }
